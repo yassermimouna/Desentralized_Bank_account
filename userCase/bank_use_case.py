14b56bd438b5a88221fddb5949811a69adb61745
yasser mimouna <yesser.mimouna20@gmail.com>
2022-12-23 01:36:46 +0100

    add deposit functions

--- userCase/bank_use_case.py
+++ userCase/bank_use_case.py
@@ -9,13 +9,13 @@ from brownie import network, accounts, exceptions
 import pytest
 
 
-def test_can_fund_and_withdrow():
+def test_my_bank():
     account = get_account_info()
     fund_me = deploy_desentralizedBank()
-    entrance_fee = fund_me.getEntranceFee() + 100
-    tx = fund_me.fund({"from": account, "value": entrance_fee})
-    tx.wait(1)
+    entrance_fee = fund_me.getEntranceFee() + 80
+    txs = fund_me.fund({"from": account, "value": entrance_fee})
+    txs.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == entrance_fee
-    tx2 = fund_me.withdrow({"from": account})
-    tx2.wait(1)
+    txsAux = fund_me.withdrow({"from": account})
+    txsAux.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == 0
